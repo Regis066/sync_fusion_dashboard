8e10ea0ea2da9299246b21b3bc3bc8ed133a7a90
Regis066 <mugisharegis72@gmail.com>
2023-08-02 16:35:37 +0200

    added navbar buttons

--- src/contexts/ContextProvider.jsx
+++ src/contexts/ContextProvider.jsx
@@ -1,27 +1,37 @@
-import React , {createContext , useContext, useState} from 'react'
+import React, { createContext, useContext, useState } from 'react';
 
-const StateContext =  createContext()
+const StateContext = createContext();
 
 const initialState = {
-    chat: false,
-    cart: false,
-    notification: false,
-    userProfile: false,
-}
+  chat: false,
+  cart: false,
+  notification: false,
+  userProfile: false,
+};
 
-export const ContextProvider = ({ children }) =>{
-    const [activeMenu, setActiveMenu] = useState(true)
-        return (
-            <StateContext.Provider
-            value={{
-                activeMenu,
-                setActiveMenu,
+export const ContextProvider = ({ children }) => {
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setIsClicked] = useState(initialState);
+  const [screenSize, setScreenSize] = useState(undefined)
 
-            }}>
-            {children}
-            
-            </StateContext.Provider>
-        )
-}
+  const handleClick = (clicked) =>{
+    setIsClicked({...initialState, [clicked]: true})
+  }
 
-export const useStateContext = () => useContext(StateContext)
+  return (
+    <StateContext.Provider
+      value={{
+        activeMenu,
+        setActiveMenu,
+        isClicked,
+        setIsClicked,
+        handleClick,
+        screenSize, setScreenSize
+      }}
+    >
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Use 'react-dom' instead of 'react-dom/client'
+import './index.css';
+import App from './App';
 import { ContextProvider } from './contexts/ContextProvider';
 
-const rootElement = document.getElementById('root')
-const root = ReactDOM.createRoot(rootElement)
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
 
 root.render(
-    <ContextProvider>
+  <ContextProvider>
     <App />
-    </ContextProvider>
-)
+  </ContextProvider>
+);
